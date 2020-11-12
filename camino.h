e0d26e655cceb255ad3326ea54c0d9255cefc552
luiisdelafuente <73260555+luiisdelafuente@users.noreply.github.com>
2020-11-12 16:53:33 -0300

    Add files via upload

--- camino.h
+++ camino.h
@@ -2,6 +2,10 @@
 #pragma once
 
 #include "inicio.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 
 class camino: public QWidget
 {
@@ -16,11 +20,18 @@ private:
 
 public:
 	camino(FILE*FP,QWidget * parent=0);
+	FILE *FPP;
 
 public slots:
 	void refrescarventana();
-	void cerrar();
 
+	//Camino 1
+	void ventana3();
+	void ventana4();
+	void final1();
+	void final2();
+
+	//Camino 2
 };
 
 
